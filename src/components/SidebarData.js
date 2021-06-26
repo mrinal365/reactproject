d68dfd61cdb8531da8728f7b1063aeca786e6bbf
mrinal365 <40198111+mrinal365@users.noreply.github.com>
2021-06-26 12:23:59 +0530

    Add files via upload

---
+++ src/components/SidebarData.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Recovery',
+    path: '/recovery',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+ 
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../App.css';
+import'./Home.css';
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
+
+export default Home;
